80d369c89cfb9d701940548051923c3c23c9afea
jamesr <jameselliot@gmail.com>
2013-02-26 21:33:22 +0000

    Adding comments to Webpage class

--- src/main/scala/howbigisthewebpage/Webpage.scala
+++ src/main/scala/howbigisthewebpage/Webpage.scala
@@ -1,8 +1,18 @@
 package howbigisthewebpage
 
+/**
+ * Represents a webpage in the system.  Created with the URL (minus protocol) and an apache commons httpclient
+ */
 class Webpage(url : String, httpclient : org.apache.http.client.HttpClient) {
+  /**
+   * The error response from the download method.
+   */
   val ERROR_RESPONSE = "*"
-    
+   
+  /**
+   * download the webpage at the URL given during construction.  Returns either a string representing the 
+   * content length downloaded, or the error response if an error occurs.
+   */
   def download : String = {
     try {
 	    val get = new org.apache.http.client.methods.HttpGet("http://" + url)
