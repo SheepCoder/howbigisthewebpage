3420bff1bd238897a69e137c938a0fee2c63d055
jamesr <jameselliot@gmail.com>
2013-02-26 08:07:48 +0000

    Adding comments to Input class

--- src/main/scala/howbigisthewebpage/Input.scala
+++ src/main/scala/howbigisthewebpage/Input.scala
@@ -2,12 +2,22 @@ package howbigisthewebpage
 
 import scala.collection.mutable.ListBuffer
 
+/**
+ * Class for handling user input.
+ */
 class Input {
 
   var sites = new ListBuffer[String]
   
+  /**
+   * Returns the number of sites read in.
+   */
   def size() : Int = sites.size
   
+  /**
+   * Reads from the given input one line at a time, each line is considered a site.
+   * Will continue to read until a line contains "-1".
+   */
   def read(reader: java.io.Reader) = {
     val buffer = createBufferedReader(reader);
     var line : String = buffer.readLine()
@@ -20,5 +30,8 @@ class Input {
   
   private def createBufferedReader(reader : java.io.Reader) : java.io.BufferedReader = new java.io.BufferedReader(reader)
   
+  /**
+   * Return the site at the given index.
+   */
   def get(index : Int) : String = sites(index)
 }
