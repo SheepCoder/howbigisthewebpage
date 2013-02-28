2198ea3099630a55346d87ca8109b2886591a3e4
jamesr <jameselliot@gmail.com>
2013-02-28 20:25:34 +0000

    Add comments to BufferedInput trait

--- src/main/scala/howbigisthewebpage/BufferedInput.scala
+++ src/main/scala/howbigisthewebpage/BufferedInput.scala
@@ -1,5 +1,11 @@
 package howbigisthewebpage
 
+/**
+ * Defines how a single line can be read and returned.
+ */
 trait BufferedInput {
+  /**
+   * Read a line of text and return it.
+   */
 	def readLine() : String
 }
